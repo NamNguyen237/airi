b9e838aab1e41633e4727bcd3e753d8bbc7c7d17
IAmNamNguyen <138494161+JuniorDevNam@users.noreply.github.com>
2025-09-14 00:45:08 +0700

    Update vite.config.ts

--- apps/stage-web/vite.config.ts
+++ apps/stage-web/vite.config.ts
@@ -167,12 +167,13 @@ export default defineConfig({
     }),
   ],
   server: { // <-- Thêm hoặc chỉnh sửa block server này
-    allowedHosts: [
+    allowedHosts: 'all', // Cho phép tất cả các host (chỉ dùng trong môi trường phát triển cục bộ)
+    //  //[
       //'localhost',
       //'127.0.0.1',
       //'.ngrok-free.app' // <-- Thêm dòng này để cho phép các subdomain của ngrok
       // Hoặc thêm host cụ thể: ví dụ '3d0324e7742f.ngrok-free.app'
-      "all" // Cho phép tất cả các host (chỉ dùng trong môi trường phát triển cục bộ)
-    ]
+      
+    //]
   }
 })
